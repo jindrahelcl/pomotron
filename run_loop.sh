7d77e4a5e81d040ac2d8a970b7a87b87864eda10
Tomáš Křen <tomkren@gmail.com>
2025-08-09 15:09:02 +0200

    Adedd helper scripts, infinite loop and start_gui to be added to run from autostart

---
+++ run_loop.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+source venv/bin/activate
+cd raspitron
+export STORYTRON_URL="https://pomotron.cz"
+export RASPITRON_TTS_LANG=cs
+
+while true; do
+	./run.sh
+	echo "Pomotron exited with code $? — restarting in 2 seconds."
+	sleep 2
+done
