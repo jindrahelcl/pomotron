753703a705252b77a82003ae273bed8c26870a53
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2025-08-13 15:45:48 +0200

    added debug flag

--- storytron/run.py
+++ storytron/run.py
@@ -2,9 +2,16 @@
 """
 StoryTRON Flask Application Runner
 """
+import argparse
 from app import app
 
 if __name__ == '__main__':
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Run the StoryTRON Flask application')
+    parser.add_argument('--debug', action='store_true',
+                       help='Run in debug mode with auto-reload')
+    args = parser.parse_args()
+
     print(f"Starting StoryTRON on {app.config['HOST']}:{app.config['PORT']}")
 
     # Check OpenAI API key
@@ -18,5 +25,5 @@ if __name__ == '__main__':
     app.run(
         host=app.config['HOST'],
         port=app.config['PORT'],
-        debug=False
+        debug=args.debug
     )
