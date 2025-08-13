86eb73d6ac2e755e5f4b6d40a83bd39a4eb1fb9e
Tomas Tintera <trosos@seznam.cz>
2025-08-14 00:56:30 +0200

    use screen

--- start_gui.sh
+++ start_gui.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 TITLE="Pomotron"
-TERM_CMD="lxterminal --title=$TITLE --working-directory=$HOME/pomotron --command=\"/bin/bash -lc '$HOME/pomotron/run_loop.sh'\""
+#TERM_CMD="lxterminal --title=$TITLE --working-directory=$HOME/pomotron --command=\"/bin/bash -lc '$HOME/pomotron/run_loop.sh'\""
+TERM_CMD="lxterminal --title=$TITLE --working-directory=$HOME/pomotron --command=\"/bin/bash -lc 'screen -dmS myserver && screen -X stuff '$HOME/pomotron/run_loop.sh\\\\\\\\\\\\\\\\n' && screen -r myserver'\""
 
 # Launch terminal
 bash -lc "$TERM_CMD"
