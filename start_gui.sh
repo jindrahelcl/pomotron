61a6badfea588173885cb47808f8a20e0f4bc066
Tomáš Křen <tomkren@gmail.com>
2025-08-14 22:57:06 +0200

    gui

--- start_gui.sh
+++ start_gui.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-TERM_CMD="lxterminal --title=Pomotron --working-directory=$HOME/pomotron --command=\"screen -dmS myserver && screen -X stuff 'xdotool search --name Pomotron windowactivate; $HOME/pomotron/run_loop.sh\\\\n' && screen -r myserver\""
-
+TITLE="Pomotron"
+#TERM_CMD="lxterminal --title=$TITLE --working-directory=$HOME/pomotron --command=\"/bin/bash -lc '$HOME/pomotron/run_loop.sh'\""
+TERM_CMD="lxterminal --title=$TITLE --working-directory=$HOME/pomotron --command=\"/bin/bash -lc 'screen -dmS myserver && screen -X stuff '$HOME/pomotron/run_loop.sh\\\\\\\\\\\\\\\\n' && screen -r myserver'\""
 
 # Launch terminal
 bash -lc "$TERM_CMD"
