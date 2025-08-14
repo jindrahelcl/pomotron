4d0c8c2c0a6082221e7e786c5cc12172290c3ed7
Tomas Tintera <trosos@seznam.cz>
2025-08-14 22:32:32 +0200

    Escaping session

--- start_gui.sh
+++ start_gui.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
 
-TERM_CMD="lxterminal --title=Pomotron --working-directory=$HOME/pomotron --command=\"screen -dmS myserver && screen -X stuff 'xdotool search --name Pomotron windowactivate; $HOME/pomotron/run_loop.sh\\\\n' && screen -r myserver\""
-
-
-# Launch terminal
-bash -lc "$TERM_CMD"
+lxterminal --title=Pomotron --working-directory=/home/trosos/pomotron --command='screen -dmS myserver && screen -X stuff "xdotool search --name Pomotron windowactivate; /home/trosos/pomotron/run_loop.sh\n" && screen -r myserver'
