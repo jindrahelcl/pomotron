6b2f85e71f1848505e4670501f0e554e229c07b3
Tomáš Křen <tomkren@gmail.com>
2025-08-14 22:36:03 +0200

    Revert "Escaping session"
    This reverts commit 4d0c8c2c0a6082221e7e786c5cc12172290c3ed7.

--- start_gui.sh
+++ start_gui.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
-lxterminal --title=Pomotron --working-directory=/home/trosos/pomotron --command='screen -dmS myserver && screen -X stuff "xdotool search --name Pomotron windowactivate; /home/trosos/pomotron/run_loop.sh\n" && screen -r myserver'
+TERM_CMD="lxterminal --title=Pomotron --working-directory=$HOME/pomotron --command=\"screen -dmS myserver && screen -X stuff 'xdotool search --name Pomotron windowactivate; $HOME/pomotron/run_loop.sh\\\\n' && screen -r myserver\""
+
+
+# Launch terminal
+bash -lc "$TERM_CMD"
